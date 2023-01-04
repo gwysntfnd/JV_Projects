0e8c96c81b676c34db4f113f5eb5612f04cb5c2c
schtz <112069267+gwysntfnd@users.noreply.github.com>
2023-01-04 22:41:28 +0800

    Add files via upload

---
+++ loopPasswordMessage.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String password = "sti";
+	    int x;
+	    if (password == "sti") {
+	        for(x = 0; x<=5; x++) {
+	            System.out.println ("correct");
+	        }
+	    }
+		else {
+		    for (x=0; x<=10; x++) {
+		        System.out.println("incorrect");
+		    }
+		}
+	}
+}

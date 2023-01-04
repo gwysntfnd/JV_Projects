052338ff5b14519f3c0908f9095103e8ce6ec5e7
schtz <112069267+gwysntfnd@users.noreply.github.com>
2023-01-04 22:46:28 +0800

    Add files via upload

---
+++ forLogIn.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main
+    {
+        public static void main(String[] args)
+            {
+                Scanner var1 = new Scanner(System.in);
+                System.out.println("Login:");
+                String loginName = var1.nextLine();
+                    //condition
+                        if(loginName.equals("asdfg"))
+                            {
+                                //right
+                                int correct = 1;
+                                    for(int for1 =1; for1 <=correct; for1++)
+                                        {
+                                            {
+                                                System.out.print("valid");
+                                            }
+                                                System.out.println();
+                                        }
+                                }
+                        else
+                            {                                       
+                                //wrong
+                                int incorrect = 1;
+                                    for(int for2 =1; for2 <=incorrect; for2++)
+                                        {
+                                            {
+                                                System.out.print("not valid");
+                                            }
+                                                System.out.println();
+                                        }
+                            }
+            }
+    }
